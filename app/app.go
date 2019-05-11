39130e48ab558e4db7616a98558e74b47e7fd7f4
Daniel Schulz <d.schulz93@gmx.de>
2019-05-11 16:36:27 +0200

    Read subtitles

--- app/app.go
+++ app/app.go
@@ -1,45 +1,60 @@
 package main
 
 import (
-	"cloud.google.com/go/translate"
-	"context"
 	"encoding/json"
 	"fmt"
-	"log"
+	"io/ioutil"
 )
 
 type Vocabulary struct {
-	NativeLanguages []struct {
-		NativeLanguage  string `json:"nativeLanguage"`
-		TargetLanguages []struct {
-			TargetLanguage string `json:"targetLanguage"`
-			Series         []struct {
-				Name    string `json:"name"`
-				Seasons struct {
-					Season   int `json:"season"`
-					Episodes []struct {
-						Episode    int `json:"episode"`
-						Vocabulary []struct {
-							Original    string `json:"original"`
-							Translation string `json:"translation"`
-							Seconds     int    `json:"seconds"`
-						} `json:"vocabulary"`
-					} `json:"episodes"`
-				} `json:"seasons"`
-			} `json:"series"`
-		} `json:"targetLanguages"`
-	} `json:"nativeLanguages"`
+	NativeLanguages []NativeLanguage `json:"nativeLanguages"`
+}
+
+type NativeLanguage struct {
+	NativeLanguage  string           `json:"nativeLanguage"`
+	TargetLanguages []TargetLanguage `json:"targetLanguages"`
+}
+
+type TargetLanguage struct {
+	TargetLanguage string   `json:"targetLanguage"`
+	Series         []Series `json:"series"`
+}
+
+type Series struct {
+	Name    string `json:"name"`
+	Seasons Season `json:"seasons"`
+}
+
+type Season struct {
+	Season   int       `json:"season"`
+	Episodes []Episode `json:"episodes"`
+}
+
+type Episode struct {
+	Episode    int       `json:"episode"`
+	Vocabulary []Vocable `json:"vocabulary"`
+}
+
+type Vocable struct {
+	Original    string `json:"original"`
+	Translation string `json:"translation"`
+	Seconds     int    `json:"seconds"`
+}
+
+type Path struct {
+	NativeLanguage, TargetLanguage, Series string
+	Season, Episode                        int
 }
 
 func main() {
-	ctx := context.Background()
+	//ctx := context.Background()
 
-	_, err := translate.NewClient(ctx)
-	if err != nil {
-		log.Fatalf("Failed to create client: %v", err)
-	}
+	// _, err := translate.NewClient(ctx)
+	// if err != nil {
+	//	log.Fatalf("Failed to create client: %v", err)
+	//}
 
-	result := Vocabulary{}
+	result := getVocabulary("/go/src/github.com/DanShu93/vocabulary-collector/netflix/")
 
 	marshaled, err := json.MarshalIndent(result, "", "  ")
 	if err != nil {
@@ -47,4 +62,36 @@ func main() {
 	}
 
 	fmt.Println(string(marshaled))
+}
+
+func getVocabulary(inputPath string) Vocabulary {
+	vocabulary := Vocabulary{NativeLanguages: []NativeLanguage{}}
+	nativeLanguageNames, err := ioutil.ReadDir(inputPath)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, n := range nativeLanguageNames {
+		if n.IsDir() {
+			targetLanguages := []TargetLanguage{}
+			targetLanguageNames, err := ioutil.ReadDir(inputPath + n.Name())
+			if err != nil {
+				panic(err)
+			}
+
+			for _, t := range targetLanguageNames {
+				if t.IsDir() {
+					targetLanguages = append(targetLanguages, TargetLanguage{TargetLanguage: t.Name(), Series: []Series{}})
+
+					_, err := ioutil.ReadDir(inputPath + n.Name() + "/" + t.Name())
+					if err != nil {
+						panic(err)
+					}
+				}
+			}
+			vocabulary.NativeLanguages = append(vocabulary.NativeLanguages, NativeLanguage{NativeLanguage: n.Name(), TargetLanguages: targetLanguages})
+		}
+	}
+
+	return vocabulary
 }
